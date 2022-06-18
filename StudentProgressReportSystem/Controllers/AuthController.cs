3a0326c1de3479fa58fb248fecb400a23accbb4b
bhuwanjungthapa <bhuwanjung115@gmail.com>
2022-06-18 21:20:05 +0545

    base layout for dashboard and auth

---
+++ StudentProgressReportSystem/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentProgressReportSystem.Controllers
+{
+	public class AuthController : Controller
+	{
+		public ViewResult Login()
+		{
+			return View();
+		}
+
+		public ViewResult Signup()
+		{
+			return View();
+		}
+		
+	}
+}
