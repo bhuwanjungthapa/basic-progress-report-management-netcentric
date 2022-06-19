1639e6daeaccf7dc60d6c75536e45d7ca1bccb07
bhuwanjungthapa <bhuwanjung115@gmail.com>
2022-06-19 19:55:36 +0545

    classes setup and list + create form

---
+++ StudentProgressReportSystem/Controllers/ClassController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentProgressReportSystem.Controllers
+{
+    public class ClassController : Controller
+    {
+        // GET: /<controller>/
+        public ViewResult List()
+        {
+            return View();
+        }
+        public ViewResult Add()
+        {
+            return View();
+        }
+    }
+}
+
